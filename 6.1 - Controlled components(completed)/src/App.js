54794042f146be3307a4e2c05632c5519d0c5c75
karthik-kakarla <karthik.nani02@gmail.com>
2024-03-12 22:42:40 +0530

    React js controlled components

---
+++ 6.1 - Controlled components(completed)/src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MessageBox from './MessageBox';
+
+const App = () => <MessageBox nameVal="John Doe" natureVal="Tech Support" queryVal="Please respond I have been facing problem in login my account"/>;
+
+export default App;
